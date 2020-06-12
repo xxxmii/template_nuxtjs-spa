42071351e549ae4a3b135be2f11e4a5553124f07
xxxmii <64941033+xxxmii@users.noreply.github.com>
2020-06-12 11:00:14 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500;700&display=swap' },
     ]
   },
   /*
